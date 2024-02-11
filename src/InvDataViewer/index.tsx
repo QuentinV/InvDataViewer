35d76636904a54d9e5ac85feda38d7e403923e42
Quentin Vanhauteghem <quentvht@gmail.com>
2024-02-11 19:17:43 +0100

    init

---
+++ src/InvDataViewer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const DataViewer: React.FC = () => (<div>
+    <div>
+        <h3>Income Statement</h3>
+    </div>
+    <div>
+        <h3>Balance Sheet</h3>
+    </div>
+    <div>
+        <h3>Cash Flow</h3>
+    </div>
+</div>);
