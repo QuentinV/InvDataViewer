acada949865e1d8f6607c920aa75ac2133f8673d
Quentin Vanhauteghem <quentvht@gmail.com>
2024-02-11 20:26:21 +0100

    init

--- src/InvDataViewer/index.tsx
+++ src/InvDataViewer/index.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React from 'react'
+import { Data } from './types'
 
-export const DataViewer: React.FC = () => (<div>
-    <div>
-        <h3>Income Statement</h3>
-    </div>
-    <div>
-        <h3>Balance Sheet</h3>
-    </div>
-    <div>
-        <h3>Cash Flow</h3>
-    </div>
-</div>);
+interface DataViewerProps {
+    dataPerYears: { [key: string]: Data }
+}
+
+export const DataViewer: React.FC<DataViewerProps> = ({ dataPerYears }) => {
+    return (
+        <div>
+            <div>
+                <h3>Income Statement</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            {Object.keys(dataPerYears).map((year) => (
+                                <th>{year}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+            <div>
+                <h3>Balance Sheet</h3>
+            </div>
+            <div>
+                <h3>Cash Flow</h3>
+            </div>
+        </div>
+    )
+};
