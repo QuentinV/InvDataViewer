1fbb3782f0d0d6e1bf5c34fbab8e00c2b5234b92
Quentin Vanhauteghem <quentvht@gmail.com>
2024-02-25 21:01:13 +0100

    login form

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -30,12 +30,23 @@ export const LoginPage: React.FC = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("login", { required: true })} />
-            <input {...register("password", { required: true })} type='password' />
-            {(errors.login || errors.password) && <span>Missing required fields</span>}
-
-            <input type="submit" />
-        </form>
+        <div className='m-auto w-3'>
+            <h2 className='text-center'>Login form</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='flex flex-column gap-2 m-3'>
+                    <label htmlFor="input-login">Username</label>
+                    <input  id='input-login' {...register("login", { required: true })} className='p-inputtext' />
+                </div>
+                <div className='flex flex-column gap-2 m-3'>
+                    <label htmlFor="input-password">Password</label>
+                    <input id='input-password' {...register("password", { required: true })} type='password' className='p-inputtext' />
+                </div>
+                <div>
+                    {(errors.login || errors.password) && <span>Missing required fields</span>}
+                </div>
+                <div className='m-3 text-center'><button type="submit" className='p-button'>Sign in</button></div>
+            </form>
+        </div>
+        
     );
 };
