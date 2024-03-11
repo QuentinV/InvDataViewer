b7f91da157cf617f679d689cee32b3fe849e84fc
Quentin Vanhauteghem <quentvht@gmail.com>
2024-03-12 00:58:11 +0100

    'change'

--- src/pages/RulesConfigPage/index.tsx
+++ src/pages/RulesConfigPage/index.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { api } from '../../api/invData';
+// @ts-expect-error no type
+import { JsonEditor as Editor } from 'jsoneditor-react';
+import 'jsoneditor-react/es/editor.min.css';
+import { Button } from 'primereact/button';
 
 export const RulesConfigPage: React.FC = () => {
-    return <div></div>
+    const [rules, setRules] = useState<object>();
+
+    useEffect(() => {
+        const getConfigs = async () => {
+            const res = await api(`invData/config/fundamentals/rules?limit=1`);
+            const value = await res.json();
+            setRules(value[0].rules);
+        }
+        getConfigs();
+    }, []);
+
+    if (!rules) return null;
+
+    const save = () => {
+        api(`invData/config/fundamentals/rules`, { method: 'POST', body: JSON.stringify({ rules }) } );
+    };
+
+    return <div>
+        <h2 className='text-center'>Edit rules config</h2>
+        <div className='m-auto'>
+            <Editor
+                value={rules}
+                onChange={(value: object) => { setRules(value) }}
+            />
+        </div>
+        <div className='mt-2 text-center'><Button label="Save" onClick={save} /></div>
+    </div>   
 }
