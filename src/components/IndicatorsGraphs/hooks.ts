2b4b263c87e480a93652de840af108016e0a0000
Quentin Vanhauteghem <quentvht@gmail.com>
2024-05-01 15:36:21 +0200

    free cashflow

--- src/components/IndicatorsGraphs/hooks.ts
+++ src/components/IndicatorsGraphs/hooks.ts
@@ -156,6 +156,9 @@ export const useChartData: (t: TFunction) => ChartValueType[][] = (t) => {
             },
             { 
                 data: ( years ) => getData( t(`${tp}.cashflowRatio.title`), t(`${tp}.cashflowRatio.dataset1`), years, { getValue: data => ((data.CASH_FLOW.NET_CASH_PROVIDED_BY_OPERATING_ACTIVITIES || 0) / (data.BALANCE_SHEET.TOTAL_CURRENT_LIABILITIES || 1)) * 100 } )
+            },
+            { 
+                data: ( years ) => getData( t(`${tp}.freeCashflow.title`), t(`${tp}.freeCashflow.dataset1`), years, { getValue: data => data.metrics.freeCashFlow } )
             }            
         ],
         [],
