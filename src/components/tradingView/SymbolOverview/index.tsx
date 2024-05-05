9afe30229e8b6f859b55886a8dec93898b2ab2cb
Quentin Vanhauteghem <quentvht@gmail.com>
2024-05-05 20:37:53 +0200

    display market share price trading view

---
+++ src/components/tradingView/SymbolOverview/index.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef, memo } from 'react'
+
+interface TradindViewSymbolOverviewProps {
+    ticker: string
+}
+
+const pTradingViewSymbolOverview: React.FC<TradindViewSymbolOverviewProps> = ({
+    ticker,
+}) => {
+    const container = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        const script = document.createElement('script')
+        script.src =
+            'https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js'
+        script.type = 'text/javascript'
+        script.async = true
+        script.innerHTML = `
+      {
+          "symbols": [ [ "NASDAQ:${ticker}|1D" ] ],
+          "chartOnly": false,
+          "locale": "en",
+          "colorTheme": "light",
+          "autosize": true,
+          "scalePosition": "right",
+          "scaleMode": "Normal",
+          "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+          "fontSize": "10",
+          "valuesTracking": "1",
+          "changeMode": "price-and-percent",
+          "chartType": "area",
+          "lineWidth": 2,
+          "lineType": 0,
+          "dateRanges": [ "1d|1", "1m|1D", "3m|60", "12m|1D", "60m|1W", "all|1M" ]
+        } `
+        container?.current?.appendChild(script)
+    }, [ticker])
+
+    return (
+        <div className="tradingview-widget-container w-full" ref={container}></div>
+    )
+}
+
+export const TradingViewSymbolOverview = memo(pTradingViewSymbolOverview)
--- src/pages/TickerPage/index.tsx
+++ src/pages/TickerPage/index.tsx
@@ -1,47 +1,58 @@
 import React, { useEffect, useState } from 'react'
 import { InvDataViewer } from '../../components/InvDataViewer'
-import { IndicatorsGraph } from '../../components/IndicatorsGraphs';
+import { IndicatorsGraph } from '../../components/IndicatorsGraphs'
 import { useParams } from 'react-router'
-import { InvData } from '../../models/types';
-import { api } from '../../api/invData';
-import { Config } from '../../components/InvDataViewer/types';
+import { InvData } from '../../models/types'
+import { api } from '../../api/invData'
+import { Config } from '../../components/InvDataViewer/types'
+import { useTranslation } from 'react-i18next'
+import { TradingViewSymbolOverview } from '../../components/tradingView/SymbolOverview'
 
 export const TickerPage: React.FC = () => {
+    const { t } = useTranslation()
     const { ticker } = useParams()
     const [data, setData] = useState<InvData | undefined | null>()
-    const [configs, setConfigs] = useState<Config[] | undefined>();
+    const [configs, setConfigs] = useState<Config[] | undefined>()
 
     useEffect(() => {
         const getTicker = async () => {
-            const res = await api(`invData/fundamentals/${ticker}`);
-            if ( res.status === 404 ) {
+            const res = await api(`invData/fundamentals/${ticker}`)
+            if (res.status === 404) {
                 setData(null)
             } else {
-                setData(await res.json());
-            }            
+                setData(await res.json())
+            }
         }
-        getTicker();
+        getTicker()
         const getConfig = async () => {
-            const res = await api(`invData/config/fundamentals/display`);
-            setConfigs(await res.json());
+            const res = await api(`invData/config/fundamentals/display`)
+            setConfigs(await res.json())
         }
-        getConfig();
-    }, [ticker]);
+        getConfig()
+    }, [ticker])
 
-    if ( data === undefined || configs === undefined ) {
-        return null;
+    if (data === undefined || configs === undefined) {
+        return null
     }
 
-    if ( data === null ) {
-        return <div className="m-5 text-center">
-            <h2>{ticker}</h2>
-            <div className='text-orange-500'>Data not found</div>
+    if (data === null) {
+        return (
+            <div className="m-5 text-center">
+                <h2>{ticker}</h2>
+                <div className="text-orange-500">Data not found</div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="m-5">
+            <h1 className="text-center">{data.name}</h1>
+            <div>
+                <h3 className="bg-primary p-2">{t('ticker.market.title')}</h3>
+                <TradingViewSymbolOverview ticker={ticker || ''} />
+            </div>
+            <IndicatorsGraph data={data} />
+            <InvDataViewer data={data} configs={configs} />
         </div>
-    }
-
-    return <div className="m-5">
-        <h1 className="text-center">{data.name}</h1>
-        <IndicatorsGraph data={data} />
-        <InvDataViewer data={data} configs={configs} />
-    </div>
+    )
 }
