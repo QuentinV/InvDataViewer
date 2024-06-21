8f9fe0d2c3479b1da06268febe3bf8ceea415259
Quentin Vanhauteghem <quentvht@gmail.com>
2024-06-21 22:35:42 +0200

    add documentation for charts config

--- src/pages/ConfigPage/MetricsFormulasConfig/index.tsx
+++ src/pages/ConfigPage/MetricsFormulasConfig/index.tsx
@@ -25,22 +25,22 @@ export const MetricsFormulasConfig: React.FC = () => {
                     <div className='w-6'>
                         <div>Global metrics (metrics)</div>
                         <ul className='mt-1'>
-                            <li><b>f</b>: access any formula. Ex: <b>f.ROUND( 2.25, 1 )</b></li>
-                            <li><b>metrics</b>: Read any previously calculated gobal metrics</li>
-                            <li><b>firstYear</b> and <b>lastYear</b>: Get year as number</li>
-                            <li><b>fy</b> and <b>ly</b>: Access data of first or last year. Ex: <b>fy.metrics</b></li>
-                            <li><b>years</b>: array of all years - [ 2014, 2015, 2016, .., 2024 ]</li>
-                            <li><b>row</b>: function to return array of data for a field or metric useful for <b>f.SUM</b><br /><span className='ml-5'>Ex: <b>f.SUM(row(&apos;metrics.investmentMargin&apos;))</b></span></li>
+                            <li><b className='text-primary'>f</b>: access any formula. Ex: <b>f.ROUND( 2.25, 1 )</b></li>
+                            <li><b className='text-primary'>metrics</b>: Read any previously calculated gobal metrics</li>
+                            <li><b className='text-primary'>firstYear</b> and <b className='text-primary'>lastYear</b>: Get year as number</li>
+                            <li><b className='text-primary'>fy</b> and <b className='text-primary'>ly</b>: Access data of first or last year. Ex: <b>fy.metrics</b></li>
+                            <li><b className='text-primary'>years</b>: array of all years - [ 2014, 2015, 2016, .., 2024 ]</li>
+                            <li><b className='text-primary'>row</b>: function to return array of data for a field or metric useful for <b>f.SUM</b><br /><span className='ml-5'>Ex: <b>f.SUM(row(&apos;metrics.investmentMargin&apos;))</b></span></li>
                         </ul>
                     </div>
                     <div className='w-6'>
                         <div>Metrics per year (year.metrics)</div>
                         <ul className='mt-1'>
-                            <li><b>f</b>: access any formula. Ex: <b>f.ROUND( 2.25, 1 )</b></li>
-                            <li><b>INCOME_STATEMENT</b>, ... : Data from fundamentals</li>
-                            <li><b>metrics</b>: Read any previously calculated metrics for the same year</li>
-                            <li><b>firstYear</b> and <b>lastYear</b>: Get year as number</li>
-                            <li><b>py</b>: Get values of previous year. <br /><span className='ml-4'>Ex: <b>py.INCOME_STATEMENT</b>, ...</span></li>
+                            <li><b className='text-primary'>f</b>: access any formula. Ex: <b>f.ROUND( 2.25, 1 )</b></li>
+                            <li><b className='text-primary'>INCOME_STATEMENT</b>, ... : Data from fundamentals</li>
+                            <li><b className='text-primary'>metrics</b>: Read any previously calculated metrics for the same year</li>
+                            <li><b className='text-primary'>firstYear</b> and <b className='text-primary'>lastYear</b>: Get year as number</li>
+                            <li><b className='text-primary'>py</b>: Get values of previous year. <br /><span className='ml-4'>Ex: <b>py.INCOME_STATEMENT</b>, ...</span></li>
                         </ul>
                     </div>
                 </div>
