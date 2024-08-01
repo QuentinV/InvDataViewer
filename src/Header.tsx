8e3b6a642a30ba6ffb216f7bba84e5d049376fda
Quentin Vanhauteghem <quentvht@gmail.com>
2024-08-02 01:08:39 +0200

    feat: top right navs icons for company page to scroll to categories

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import { Menubar } from 'primereact/menubar'
 import i18next from 'i18next'
