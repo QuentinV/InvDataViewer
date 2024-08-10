b08c954891dec7e3a96c5019f13e157d930b314b
Quentin Vanhauteghem <quentvht@gmail.com>
2024-08-10 17:27:36 +0200

    add level selector to intrinsic value graph + warning sign in case of incomplete data

--- src/components/IntrinsicValueGraph/index.tsx
+++ src/components/IntrinsicValueGraph/index.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 
 interface IntrinsicValueGraphProps {
-  areas?: { limit: number }[];
+  areas?: number[];
   value?: number;
   height?: number;
 }
 
 const colors = [ '#758B98', '#9CA9B3', '#E5D9CD', '#AB8886', '#8C5A58' ];
+const defaultAreas = [ 20, 40, 60, 80, 100 ];
 
 export const IntrinsicValueGraph: React.FC<IntrinsicValueGraphProps> = ({ areas, value, height = 120 }) => {
-    if ( !areas || !value ) return null;
-
-    const lastAreaValue = areas[areas.length-1].limit;
-    const maxValue = value > lastAreaValue ? lastAreaValue : value;
-
+    const myAreas = areas || defaultAreas;
+    const lastAreaValue = myAreas[myAreas.length-1];
+    const maxValue = !value ? null : value > lastAreaValue ? lastAreaValue : value;
+    const valueX = maxValue ? Number((maxValue * 100 / lastAreaValue).toFixed(2)) : undefined;
+   
     let prevX = 0;
-    const valueX = value ? Number((maxValue * 100 / lastAreaValue).toFixed(2)) : 0;
-
     return (
     <svg width="100%" height={height} overflow="visible">
-        {areas.map(({limit}, index) => {
+        {myAreas.map((limit, index) => {
             const v = Number((limit * 100 / lastAreaValue).toFixed(2));
             const w = (v - prevX) + '%';
             const x = prevX;
@@ -34,7 +33,7 @@ export const IntrinsicValueGraph: React.FC<IntrinsicValueGraphProps> = ({ areas,
                         height={height-22}
                         fill={colors[index]}
                     />
-                    { index < areas.length-1 && 
+                    { !!areas && index < myAreas.length-1 && 
                     <svg viewBox={`0 0 20 12`} y={height-8} width="20" height="12" x={v+'%'} overflow="visible">
                         <text
                             x="-15"
@@ -49,7 +48,7 @@ export const IntrinsicValueGraph: React.FC<IntrinsicValueGraphProps> = ({ areas,
                 </g>
             )
         })}
-        {(value ?? null) !== null && (
+        {!!areas && (valueX ?? null) !== null && (
             <svg viewBox={`0 0 20 ${height-22}`} y="0" width="20" height={height-22} x={valueX+'%'} overflow="visible">
                 <rect x="-10" y="0" width="20" height="80" fill="#3A596B" />
                 <polygon points="-10,80 10,80 0,98" fill="#3A596B" />
@@ -58,6 +57,22 @@ export const IntrinsicValueGraph: React.FC<IntrinsicValueGraphProps> = ({ areas,
                 </text>
             </svg>
         )}
+        {!areas && 
+            (<g>
+                <rect x="0" y="0" width="100%" height={height-22} fill="#BBB" fillOpacity="0.6" />
+                <svg viewBox={`0 0 1 ${height-22}`} y="5%" width="80" height={height-22} x="50%" overflow="visible">
+                    <svg fill="#FFF" width="80" height="80" viewBox="0 0 554.2 554.199" x="-80">
+                    <g>
+                        <path d="M538.5,386.199L356.5,70.8c-16.4-28.4-46.7-45.9-79.501-45.9c-32.8,0-63.1,17.5-79.5,45.9L12.3,391.6
+                            c-16.4,28.4-16.4,63.4,0,91.8C28.7,511.8,59,529.3,91.8,529.3H462.2c0.101,0,0.2,0,0.2,0c50.7,0,91.8-41.101,91.8-91.8
+                            C554.2,418.5,548.4,400.8,538.5,386.199z M316.3,416.899c0,21.7-16.7,38.3-39.2,38.3s-39.2-16.6-39.2-38.3V416
+                            c0-21.601,16.7-38.301,39.2-38.301S316.3,394.3,316.3,416V416.899z M317.2,158.7L297.8,328.1c-1.3,12.2-9.4,19.8-20.7,19.8
+                            s-19.4-7.7-20.7-19.8L237,158.6c-1.3-13.1,5.801-23,18-23H299.1C311.3,135.7,318.5,145.6,317.2,158.7z"/>
+                    </g>
+                    </svg>
+                </svg>
+            </g>)
+        }
     </svg>
     );
 };
--- src/pages/CompanyPage/IntrinsicValue/index.tsx
+++ src/pages/CompanyPage/IntrinsicValue/index.tsx
@@ -5,6 +5,7 @@ import { IntrinsicValueGraph } from '../../../components/IntrinsicValueGraph';
 import { useUnit } from 'effector-react';
 import { companyValuesStores } from '../../../models/company/values';
 import { api } from '../../../api/invData';
+import { SelectButton } from 'primereact/selectbutton';
 
 interface IntrinsicValueProps {
     ticker: string;
@@ -15,27 +16,41 @@ export const IntrinsicValue: React.FC<IntrinsicValueProps> = ({ ticker }) => {
     const titleRef = useRef(null);
     const companyValues = useUnit(companyValuesStores.$values);
     const [price, setPrice] = useState<number>(0);
-    const areas = companyValues?.areas?.[0];
+    const [level, setLevel] = useState<number>(2);
     
     useEffect(() => {
         navs.setRef({ key: 'companyIntrinsicValueRef', ref: titleRef });
     }, []);
     
+    
+    const areas = companyValues?.areas?.[level];
     useEffect( () => {
+        if ( !areas || price ) return;
         const getPrice = async () => {
             const p = await api(`invData/tickers/${ticker}/prices`);
             setPrice(p.price);
         }
         getPrice();
-    }, [ticker]);
-
-    if ( !price ) return null; 
+    }, [ticker, areas]);
+    
+    const items = companyValues?.areas?.map( (_, i) => ({ name: t(`ticker.value.levels.${i}`), value: i }));
 
     return (
         <div>
             <h3 className="bg-primary p-2" ref={titleRef}><i className='pi pi-compass mr-2' />{t('ticker.intrinsicValue.title')}</h3>
+            {!!items && 
+                (<div className='mb-3'>
+                    <SelectButton 
+                        pt={{ button: { className: "pt-1 pb-1 pl-4 pr-4 border-gray-100 text-sm font-bold" } }}
+                        value={level} 
+                        onChange={(e) => e.value !== null && setLevel(e.value)} 
+                        optionLabel="name" 
+                        options={items} 
+                    />
+                </div>)
+            }
             <div>
-                {!!areas && <IntrinsicValueGraph areas={areas.map( limit => ({ limit }))} value={price} />}
+                {<IntrinsicValueGraph areas={areas} value={price} />}
             </div>
         </div>
     )
