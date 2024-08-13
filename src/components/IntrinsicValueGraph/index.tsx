3ce1706c3ac1d7457763b7ed720786cfd5a7a22c
Quentin Vanhauteghem <quentvht@gmail.com>
2024-08-14 00:44:41 +0200

    improv: intrinsic value graph scale

--- src/components/IntrinsicValueGraph/index.tsx
+++ src/components/IntrinsicValueGraph/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 interface IntrinsicValueGraphProps {
   areas?: number[];
@@ -9,17 +10,23 @@ interface IntrinsicValueGraphProps {
 const colors = [ '#758B98', '#9CA9B3', '#E5D9CD', '#AB8886', '#8C5A58' ];
 const defaultAreas = [ 20, 40, 60, 80, 100 ];
 
-export const IntrinsicValueGraph: React.FC<IntrinsicValueGraphProps> = ({ areas, value, height = 120 }) => {
+export const IntrinsicValueGraph: React.FC<IntrinsicValueGraphProps> = ({ areas, value, height = 150 }) => {
+    const { t } = useTranslation();
     const myAreas = areas || defaultAreas;
     const lastAreaValue = myAreas[myAreas.length-1];
-    const maxValue = !value ? null : value > lastAreaValue ? lastAreaValue : value;
-    const valueX = maxValue ? Number((maxValue * 100 / lastAreaValue).toFixed(2)) : undefined;
-   
+
+    const maxValue = (value && value > lastAreaValue ? value : lastAreaValue) * 1.1;
+    const valueX = value ? Number((value * 100 / maxValue).toFixed(2)) : undefined;
+    
     let prevX = 0;
+    let prevXLegend = 20;
     return (
     <svg width="100%" height={height} overflow="visible">
-        {myAreas.map((limit, index) => {
-            const v = Number((limit * 100 / lastAreaValue).toFixed(2));
+        {!!areas && <text x="0" y={height-38} fill="black" fontSize="12">0</text>}
+        {myAreas.slice(0, 4).map((limit, index) => {
+            if ( limit < 0 ) 
+                return <g key={index}></g>;
+            const v = Number((limit * 100 / maxValue).toFixed(2));
             const w = (v - prevX) + '%';
             const x = prevX;
             prevX = v;
@@ -30,11 +37,11 @@ export const IntrinsicValueGraph: React.FC<IntrinsicValueGraphProps> = ({ areas,
                         x={x+'%'}
                         y="0%"
                         width={w}
-                        height={height-22}
+                        height={height-52}
                         fill={colors[index]}
                     />
-                    { !!areas && index < myAreas.length-1 && 
-                    <svg viewBox={`0 0 20 12`} y={height-8} width="20" height="12" x={v+'%'} overflow="visible">
+                    {!!areas && 
+                    <svg viewBox={`0 0 20 12`} y={height-38} width="20" height="12" x={v+'%'} overflow="visible">
                         <text
                             x="-15"
                             y="0"
@@ -44,12 +51,23 @@ export const IntrinsicValueGraph: React.FC<IntrinsicValueGraphProps> = ({ areas,
                         { limit.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }) }
                         </text> 
                     </svg>
-                    }
+                    } 
                 </g>
             )
         })}
+        
+        <g>
+            <rect
+                x={(lastAreaValue > 0 ? prevX : 0) +'%'}
+                y="0%"
+                width={(100 - prevX) + '%'}
+                height={height-52}
+                fill={colors[4]}
+            />
+        </g>
+
         {!!areas && (valueX ?? null) !== null && (
-            <svg viewBox={`0 0 20 ${height-22}`} y="0" width="20" height={height-22} x={valueX+'%'} overflow="visible">
+            <svg viewBox={`0 0 20 ${height-52}`} y="0" width="20" height={height-52} x={valueX+'%'} overflow="visible">
                 <rect x="-10" y="0" width="20" height="80" fill="#3A596B" />
                 <polygon points="-10,80 10,80 0,98" fill="#3A596B" />
                 <text x="15" y="5" fill="white" fontSize="13" transform='rotate(90)'>
@@ -59,8 +77,8 @@ export const IntrinsicValueGraph: React.FC<IntrinsicValueGraphProps> = ({ areas,
         )}
         {!areas && 
             (<g>
-                <rect x="0" y="0" width="100%" height={height-22} fill="#BBB" fillOpacity="0.6" />
-                <svg viewBox={`0 0 1 ${height-22}`} y="5%" width="80" height={height-22} x="50%" overflow="visible">
+                <rect x="0" y="0" width="100%" height={height-52} fill="#BBB" fillOpacity="0.6" />
+                <svg viewBox={`0 0 1 ${height-32}`} y="5%" width="80" height={height-52} x="50%" overflow="visible">
                     <svg fill="#FFF" width="80" height="80" viewBox="0 0 554.2 554.199" x="-80">
                     <g>
                         <path d="M538.5,386.199L356.5,70.8c-16.4-28.4-46.7-45.9-79.501-45.9c-32.8,0-63.1,17.5-79.5,45.9L12.3,391.6
@@ -73,6 +91,19 @@ export const IntrinsicValueGraph: React.FC<IntrinsicValueGraphProps> = ({ areas,
                 </svg>
             </g>)
         }
+        {myAreas.map( (_v, index) => {
+            const tx = t(`ticker.intrinsicValue.areas.${index}`);
+            const x = prevXLegend;
+            prevXLegend = tx.length * 5.5 + 50 + prevXLegend;
+            return (
+            <svg key={index} x={x} y={height-21}>
+                <rect  width="10" height={10} fill={colors[index]} />
+                <text x="15" y="9" fill={colors[index]} fontSize="13">
+                    {tx}
+                </text>
+            </svg>
+            )} 
+        )}
     </svg>
     );
 };
