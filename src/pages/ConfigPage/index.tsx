072bf3def35ea28c36d95887a120537f5204c0d2
Quentin Vanhauteghem <quentvht@gmail.com>
2024-09-26 15:19:38 +0200

    improv: documentatin for config company value

--- src/pages/ConfigPage/index.tsx
+++ src/pages/ConfigPage/index.tsx
@@ -46,6 +46,10 @@ export const ConfigPage: React.FC = () => {
             </TabPanel>
             <TabPanel header="Company value">
                 <div className='m-0'>
+                    <div className='mb-2'>
+                        Each formula are executed for each level. Usually 4. For each new element it is possible to create configs with default value for each level (therefore array of 4 values).
+                        <br/>Configs can be used in formula with <b>configs.keyName</b> it will use the value of the level 0 to 3 automatically.
+                    </div>
                     <ConfigEditor endpoint='companies/values/rules' />
                 </div>
             </TabPanel>
