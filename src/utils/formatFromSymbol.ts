0342f211ad9530ad820a2648eb966be0366a1382
Quentin Vanhauteghem <quentvht@hotmail.com>
2024-12-21 22:02:27 +0100

    feat(companyvalue): format number based on selected language for company value

--- src/utils/formatFromSymbol.ts
+++ src/utils/formatFromSymbol.ts
@@ -1,12 +1,12 @@
 import { formatPercent } from './formatPercent'
 
-const defaultFormat = (value?: number) =>
-    value?.toLocaleString(undefined, {
+const defaultFormat = (language: string, value?: number) =>
+    value?.toLocaleString(language, {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2,
     })
 
-export const formatFromSymbol = (symbol?: string, value?: number) => {
+export const formatFromSymbol = (language: string, symbol?: string, value?: number) => {
     const v = symbol === '%' ? formatPercent(value) : value;
-    return `${defaultFormat(v)}${(symbol?.indexOf('%') ?? -1) !== -1 ? '%' : symbol === '$' ? '$' : '' }`;
+    return `${defaultFormat(language, v)}${(symbol?.indexOf('%') ?? -1) !== -1 ? '%' : symbol === '$' ? '$' : '' }`;
 }
